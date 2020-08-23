aeb76e5999e15131cdf16f171fd25d1a38a6a0a3
Connor Hewett <clooneyjh@gmail.com>
2020-08-23 17:36:35 +1200

    Refactoring joystick code

--- joystick.js
+++ joystick.js
@@ -1,47 +1,40 @@
 var robot = require("robotjs");
 var five = require("johnny-five")
-var board = new five.Board();
+var board = new five.Board({port: "COM3"});
 
 var mouse;
+
+function joy_move(axis_pos, mouse_pos) {
+  // Minimum threshold to count joystick movement
+  var joy_min_thresh = 0.02;
+  // Factor to move the mouse by
+  var joy_move_factor = 10;
+
+  if (axis_pos > joy_min_thresh) {
+    return(mouse_pos + joy_move_factor);
+  } else if (axis_pos < -joy_min_thresh) {
+    return(mouse_pos - joy_move_factor);
+  } else {
+    return(mouse_pos);
+  }
+}
 
 board.on("ready", function() {
 
   // Create a new `joystick` hardware instance.
   var joystick = new five.Joystick({
-    // [ X, Y]
-    pins: ["A0", "A1"]
+    pins: ["A0", "A1"] // [ X, Y]
   });
 
   joystick.on("change", function() {
-    if (this.x > 0.02 || this.x < -0.02 || this.y > 0.02 || this.y < -0.02) {
-      console.log("Joystick");
-      console.log("  x : ", this.x);
-      console.log("  y : ", this.y);
-      console.log("--------------");
 
-      mouse = robot.getMousePos();
-      console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
-      
-      if (this.x > 0.02) {
-        var new_x = mouse.x - 1;
-      } else if (this.x < -0.02) {
-        var new_x = mouse.x + 1;
-      } else {
-        var new_x = mouse.x;
-      }
+    mouse = robot.getMousePos();
+    var new_x = joy_move(this.x, mouse.x)
+    var new_y = joy_move(-this.y, mouse.y)
 
-      if (this.y > 0.02) {
-        var new_y = mouse.y - 1;
-      } else if (this.y < -0.02) {
-        var new_y = mouse.y + 1;
-      } else {
-        var new_y = mouse.y;
-      }
+    console.log("Mouse is at x:" + new_x + " y:" + new_y);
 
-      console.log("Mouse is at x:" + new_x + " y:" + new_y);
-
-      //Move the mouse to x, y on the screen.
-      robot.moveMouse(new_x, new_y);
-    }
+    //Move the mouse to x, y on the screen.
+    robot.moveMouse(new_x, new_y);
   });
 });
